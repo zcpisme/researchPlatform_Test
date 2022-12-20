55548441d9837ab0249bb8ca24ed8a2a9113a68f
zcpisme <89170273+zcpisme@users.noreply.github.com>
2022-12-19 22:53:25 -0800

    Update platfrom.py

--- platfrom.py
+++ platfrom.py
@@ -10,42 +10,16 @@ import pandas as pd
 
 researchDf = pd.read_csv('data/researchDf.csv')
 
-mycatList = ['All']
+mycatList = ['N/A']
 mycatList.extend(researchDf['category'].unique())
 category = st.selectbox('Which Category Are You Looking for?', mycatList)
 
-if category == 'All':
-    test= researchDf
-
-else:
-    df = researchDf[researchDf['category'] == category]
-    test = df[~df['person'].duplicated()]
-
-mysubcatList = ['All']
-mysubcatList.extend(test['subcategory'].unique())
-subcategory = st.selectbox('Which Subcategory Are You Looking for?', mysubcatList)
-
-if subcategory == 'All':
-    pass
-else:
-    df = test[test['subcategory'] == subcategory]
-    test = df[~df['person'].duplicated()]
-
-mydetailList = ['All']
-mydetailList.extend(test['detail'].unique())
-detail = st.selectbox('Which Detail Are You Looking for?', mydetailList)
-
-if detail == 'All':
-    pass
-else:
-    df = test[test['detail'] == detail]
-    test = df[~df['person'].duplicated()]
-
+df = researchDf[researchDf['category'] == category]
+test = df[~df['person'].duplicated()]
 
 st.dataframe(test)
 
-
-myvarList = [' ']
+myvarList = ['N/A']
 myvarList.extend(list(researchDf.columns))
 remove_item = ['person','adb_id','Key','name','category',
                'subcategory','detail','comment']
@@ -54,13 +28,10 @@ myvar = st.selectbox('Which Variable Are You Looking for?', myvarList)
 
 try:
     showedDf = pd.DataFrame((test[myvar].value_counts().reset_index()))
-    
-    showedDf.rename(columns = {myvar:"count"}, inplace = True)
-    showedDf.rename(columns = {"index":myvar}, inplace = True)
-    #st.dataframe(showedDf)
-    #showedDf['house'] = showedDf['house'].map(lambda x: int(x[5:]))
-    total = showedDf["count"].sum()
-    showedDf['percentage'] = showedDf["count"]/total
+    showedDf.rename(columns = {"index":'house'}, inplace = True)
+    showedDf['house'] = showedDf['house'].map(lambda x: int(x[5:]))
+    total = showedDf[myvar].sum()
+    showedDf['percentage'] = showedDf[myvar]/total
     showedDf['total'] = total
     
     st.dataframe(showedDf)
