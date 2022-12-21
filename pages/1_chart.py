22ae20d967468a59bf5077c2304847b9ab39e909
zcpisme <89170273+zcpisme@users.noreply.github.com>
2022-12-21 00:11:10 -0800

    Update 1_chart.py

--- pages/1_chart.py
+++ pages/1_chart.py
@@ -34,29 +34,31 @@ with minute:
 #components.iframe("http://127.0.0.1:5500/AstroChart/project/examples/radix/radix.html", height=700)
 #print(type(t_hour))
 
-birth2 = datetime(d.year,d.month,d.day,t_hour,t_minute)
-#birth2 = datetime(2000, 12, 26 , 12, 0)
-info2 = ['shenzhen','china',birth2]
-
-
-res = myfunction.JSreadable(myfunction.getAllinfo(*info2))
-astrodata = "const data = " + str(res)
-
-location = "chartHtml/radix.html"
-#location = "testHtml.html"
-HtmlFile = open(location, 'r', encoding='utf-8')
-source_code = HtmlFile.read() 
-
-location2 = "chartHtml/astrochart.js"
-HtmlFile2 = open(location2, 'r', encoding='utf-8')
-astroChartFunction = HtmlFile2.read() 
-astroChartFunction = "<script>\n" + astroChartFunction + "\n</script>"
-#print(astroChartFunction)
-
-source_code = source_code.replace('<script src="../../build/astrochart.js"></script>', astroChartFunction)
-source_code = source_code.replace('const data = [0]', astrodata)
-
-#print(type(source_code))
-components.html(source_code, height=600)
+# =============================================================================
+# birth2 = datetime(d.year,d.month,d.day,t_hour,t_minute)
+# #birth2 = datetime(2000, 12, 26 , 12, 0)
+# info2 = ['shenzhen','china',birth2]
+# 
+# 
+# res = myfunction.JSreadable(myfunction.getAllinfo(*info2))
+# astrodata = "const data = " + str(res)
+# 
+# location = "chartHtml/radix.html"
+# #location = "testHtml.html"
+# HtmlFile = open(location, 'r', encoding='utf-8')
+# source_code = HtmlFile.read() 
+# 
+# location2 = "chartHtml/astrochart.js"
+# HtmlFile2 = open(location2, 'r', encoding='utf-8')
+# astroChartFunction = HtmlFile2.read() 
+# astroChartFunction = "<script>\n" + astroChartFunction + "\n</script>"
+# #print(astroChartFunction)
+# 
+# source_code = source_code.replace('<script src="../../build/astrochart.js"></script>', astroChartFunction)
+# source_code = source_code.replace('const data = [0]', astrodata)
+# 
+# #print(type(source_code))
+# components.html(source_code, height=600)
+# =============================================================================
 
 #st.write(myfunction.JSreadable(myfunction.getAllinfo(*info2)))
